43c131b4d80499883316dacdc931e1d7a384b23c
yaolei <373561022@qq.com>
2016-10-10 10:10:08 +0800

    增加继承View的自定义控件

---
+++ CustomViewOnDraw/app/src/main/java/com/example/yl/customviewondraw/CircleView.java
@@ -0,0 +1,72 @@
+package com.example.yl.customviewondraw;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/10/10.
+ */
+
+public class CircleView extends View {
+
+    private int mColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, Color.parseColor("#ff0000"));
+        a.recycle();
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, Color.parseColor("#ff0000"));
+        a.recycle();
+        init();
+    }
+
+    private void init() {
+        mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
+        int radius = Math.min(width / 2, height / 2);
+        canvas.drawCircle( paddingLeft + width / 2, paddingTop + height / 2, radius, mPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, 200);
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, heightSize);
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSize, 200);
+        }
+    }
+}
